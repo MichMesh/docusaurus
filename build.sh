a9ba4d26c994ca95314acdc0bad76e3525f8f5f6
yNos <kc8ijq@gmail.com>
2026-01-27 20:41:21 -0500

    This is the build part of Emersons gmrs updates

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-npm run build
+#!/bin/bash
+
+if [ -n "$1" ] ; then
+  npm run build
+  git add build/* docs/*
+  git commit -am "$@"
+else
+  echo "Please supply a commit message"
+  echo "    $0 \" this is my badass change to our ballin docs\" "
+fi
+
