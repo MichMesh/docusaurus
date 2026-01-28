23623975ebd2753496fa0b4c181e3239416db41a
yNos <kc8ijq@gmail.com>
2026-01-27 21:57:15 -0500

    adding git push to the build script

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ if [ -n "$1" ] ; then
   npm run build
   git add build/* docs/*
   git commit -am "$@"
+  git push
 else
   echo "Please supply a commit message"
   echo "    $0 \" this is my badass change to our ballin docs\" "
